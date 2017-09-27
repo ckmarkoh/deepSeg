cc05c2f0369890b3e0092f06813337524acb49f3
amigcamel <amigcamel@gmail.com>
2017-09-27 15:38:54 +0800

    explicitly specify `install_requires`

--- setup.py
+++ setup.py
@@ -1,21 +1,11 @@
 """Setup script."""
 from setuptools import setup, find_packages
-from os.path import join, abspath, dirname
-
-from pip.req import parse_requirements
-from pip.download import PipSession
-
-install_reqs = parse_requirements(
-    join(dirname(abspath(__file__)), 'requirements.txt'),
-    session=PipSession()
-)
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='deepSeg',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=reqs,
+    install_requires=['Theano==0.8.2'],
     author='ckmarkoh',
     author_email='ckmarkoh@gmail.com',
     description='A deep learning Chinese Word Segmentation toolkit',
